4718b937b54c371c989dec4248dec257c63f49e0
Student <example@example.com>
2019-12-03 10:26:45 -0800

    Adding progress

--- src/distance.js
+++ src/distance.js
@@ -19,13 +19,6 @@ export function findDistance(origins, destinations) {
     request.send();
   });
 }
-// function myMap() {
-// var mapProp= {
-//   center:new google.maps.LatLng(51.508742,-0.120850),
-//   zoom:5,
-// };
-// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-// }
 
 
 // DO NOT DELETE ***
