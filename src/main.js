5eaea00c92d48523225e5cf9fe50fc79bb20b7ac
Student <example@example.com>
2019-12-03 16:07:08 -0800

    Adding progress

--- src/main.js
+++ src/main.js
@@ -17,6 +17,20 @@ $(document).ready(function() {
     $("#intro").hide();
   });
 
+  // $("#directionsButton").click(function() {
+  //   $("#map-canvas").show();
+  //   $("#searchByPlace").hide();
+  //   window.onload = function WindowLoad(event) {
+  //     var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+  //     var myOptions = {
+  //      zoom: 8,
+  //      center: myLatlng,
+  //      mapTypeId: google.maps.MapTypeId.ROADMAP
+  //    };
+  //     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  //   };
+  // });
+
   $("#find-trails").click(function () {
     $("#resultsDiv").slideDown("ease");
     $("#hikesNearby").hide();
