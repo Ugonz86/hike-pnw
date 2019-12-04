e99f88ebe1e68fa773e3b30711328da2b3f568eb
Ugonz86 <ugonzalez86@gmail.com>
2019-12-04 09:09:42 -0800

    Add progress

---
+++ src/apis/weather.js
@@ -0,0 +1,24 @@
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
+export function findWeather(coordinates) {
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+
+    let unixTime = "1575468000";
+    let date = "2019-12-04";
+
+    const cors = "https://cors-anywhere.herokuapp.com/";
+    let url = `${cors}https://api.darksky.net/forecast/${WEATHER_API_KEY}/${coordinates},${unixTime}?date=${date}`;
+
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      }
+      else {
+        reject(Error(request.statusText));
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import $ from 'jquery';
-import { findTrails } from './trails';
-// import { findDistance } from './distance';
+import { findTrails } from './apis/trails';
+import { findWeather } from './apis/weather';
+//import { findCampgrounds } from './apis/camping';
+//import { findDistance } from './apis/distance';
+
 
 $(document).ready(function() {
 
@@ -17,36 +20,35 @@ $(document).ready(function() {
     $("#intro").hide();
   });
 
-  // $("#directionsButton").click(function() {
-  //   $("#map-canvas").show();
-  //   $("#searchByPlace").hide();
-  //   window.onload = function WindowLoad(event) {
-  //     var myLatlng = new google.maps.LatLng(-34.397, 150.644);
-  //     var myOptions = {
-  //      zoom: 8,
-  //      center: myLatlng,
-  //      mapTypeId: google.maps.MapTypeId.ROADMAP
-  //    };
-  //     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-  //   };
-  // });
-
   $("#find-trails").click(function () {
-    $("#resultsDiv").slideDown("ease");
-    $("#hikesNearby").hide();
+
     let maxHikeDistance = $("input#trail-distance").val();
-    $("input#trail-distance").val("");
 
     findTrails()
       .then((response) => {
         $("#display-results").empty();
-
         const body = JSON.parse(response);
         const trails = body.trails;
+
         for (let i in trails){
-          if (trails[i].length < maxHikeDistance){
-            $("#display-results").append(`${trails[i].name}<br>${trails[i].location}<br>${trails[i].length} mile hike<br>lat: ${trails[i].latitude}, lon: ${trails[i].longitude}<p></p>`);
+          if (trails[i].length < maxHikeDistance) {
+
+            let trailCoordinates = `${trails[i].latitude},${trails[i].longitude}`;
+
+            findWeather(trailCoordinates)/* Dark Sky Weather API. Trail coordinates are used as arguments to find weather for that area. */
+              .then((response) => {
+
+                let weatherBody = JSON.parse(response);
+                let weatherSummary = weatherBody.daily.data[0].summary;
+                let temperature = weatherBody.daily.data[0].temperatureHigh;
+
+                $("#display-results").append(`<ul><li>${trails[i].name}</li><li>${trails[i].location}</li><li>${trails[i].length} mile hike</li><li>${trails[i].latitude}</li><li>${trails[i].longitude}</li><li>${weatherSummary}</li><li>${temperature} degree high</li></ul>`);
+              },
+              function(error) {
+                console.error(`I am the error message: ${error.message}`);
+              });
           }
+
         }
       },
       function(error) {
@@ -54,22 +56,7 @@ $(document).ready(function() {
         $("#display-div").show();
       });
 
-    // findDistance()
-    //   .then((response) => {
-    //     $("#display-results").empty();
-    //
-    //     const body = JSON.parse(response);
-    //     const origin_addresses = body.origin_addresses;
-    //     const destination_addresses = body.destination_addresses;
-    //     const rows = body.rows;
-    //     for (let i in rows){
-    //       if (rows[i].length > maxHikeDistance){
-    //         $("#display-results").append(`${body.origin_addresses}<br>${body.destination_addresses}<br>${rows.elements[i].distance}<br>${rows.elements[i].duration}<p></p>`);
-    //       }
-    //     }
-    //   });
   });
-
   $("#goHome").click(function() {
     $("#resultsDiv").hide();
     $("#intro").slideDown();
@@ -81,3 +68,109 @@ $(document).ready(function() {
     $("#intro").slideDown();
   });
 });
+
+
+
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+// import $ from 'jquery';
+// import { findTrails } from './apis/trails';
+// import { findWeather } from './apis/weather';
+// // import { findDistance } from './distance';
+//
+// $(document).ready(function() {
+//   $("#searchNearby").click(function() {
+//     $("#hikesNearby").slideDown();
+//     $("#intro").hide();
+//   });
+//
+//   $("#searchDestination").click(function() {
+//     $("#searchByPlace").slideDown();
+//     $("#intro").hide();
+//   });
+//
+//   $("#find-trails").click(function () {
+//     let maxHikeDistance = $("input#trail-distance").val();
+//     findTrails()
+//       .then((response) => {
+//         $("#display-results").empty();
+//         const body = JSON.parse(response);
+//         const trails = body.trails;
+//
+//         for (let i in trails){
+//           if (trails[i].length < maxHikeDistance) {
+//
+//             let trailCoordinates = `${trails[i].latitude},${trails[i].longitude}`;
+//
+//             findWeather(trailCoordinates)
+//               .then((response) => {
+//                 let weatherBody = JSON.parse(response);
+//                 let weatherSummary = weatherBody.daily.data[0].summary;
+//                 let temperature = weatherBody.daily.data[0].temperatureHigh;
+//                 $("#display-results").append(`<ul><li>${trails[i].name}</li><li>${trails[i].location}</li><li>${trails[i].length} mile hike</li><li>${trails[i].latitude}</li><li>${trails[i].longitude}</li><li>${weatherSummary}</li><li>${temperature} degree high</li></ul>`);
+//               }
+//             }
+//           }
+//         },
+//         function(error) {
+//           $("#display-results").empty().append(`<h5>There was an error processing your request: ${error.message}</h5>`);
+//           $("#display-div").show();
+//         });
+//
+//   });
+//   $("#goHome").click(function() {
+//     $("#resultsDiv").hide();
+//     $("#intro").slideDown();
+//     // $("#display-results").html('');
+//   });
+//
+//   $("#goHome2").click(function() {
+//     $("#searchByPlace").hide();
+//     $("#intro").slideDown();
+//   });
+// });
+
+// findDistance()
+//   .then((response) => {
+//     $("#display-results").empty();
+//
+//     const body = JSON.parse(response);
+//     const origin_addresses = body.origin_addresses;
+//     const destination_addresses = body.destination_addresses;
+//     const rows = body.rows;
+//     for (let i in rows){
+//       if (rows[i].length > maxHikeDistance){
+//         $("#display-results").append(`${body.origin_addresses}<br>${body.destination_addresses}<br>${rows.elements[i].distance}<br>${rows.elements[i].duration}<p></p>`);
+//       }
+//     }
+//   });
+
+// $("#directionsButton").click(function() {
+//   $("#map-canvas").show();
+//   $("#searchByPlace").hide();
+//   window.onload = function WindowLoad(event) {
+//     var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+//     var myOptions = {
+//      zoom: 8,
+//      center: myLatlng,
+//      mapTypeId: google.maps.MapTypeId.ROADMAP
+//    };
+//     var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+//   };
+// });
+
+// findDistance()
+//   .then((response) => {
+//     $("#display-results").empty();
+//
+//     const body = JSON.parse(response);
+//     const origin_addresses = body.origin_addresses;
+//     const destination_addresses = body.destination_addresses;
+//     const rows = body.rows;
+//     for (let i in rows){
+//       if (rows[i].length > maxHikeDistance){
+//         $("#display-results").append(`${body.origin_addresses}<br>${body.destination_addresses}<br>${rows.elements[i].distance}<br>${rows.elements[i].duration}<p></p>`);
+//       }
+//     }
+//   });
