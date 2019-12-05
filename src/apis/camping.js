142983b3745c8c6ab181fd3fe78ac4b17315d3c1
Student <example@example.com>
2019-12-05 12:29:51 -0800

    Add progress

--- src/apis/camping.js
+++ src/apis/camping.js
@@ -1,21 +1,22 @@
-// const API_KEY = process.env.API_KEY;
-//
-// export function findCampgrounds(lat, long){
-//     return new Promise(function(resolve, reject) {
-//       let request = new XMLHttpRequest();
-//
-//       const endPoint = "https://www.hikingproject.com/data/get-campgrounds?";
-//       let url = `${endPoint}lat=${lat}&lon=${long}&maxDistance=50&key=${API_KEY}&maxResults=10&sort=distance`;
-//
-//       request.onload = function() {
-//         if (this.status === 200) {
-//           resolve(request.response);
-//         }
-//         else {
-//           reject(Error(request.statusText));
-//         }
-//       };
-//       request.open("GET", url, true);
-//       request.send();
-//     });
-//   }
+const API_KEY = process.env.API_KEY;
+
+export function findCampgrounds(latitude, longitude){
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+
+    const cors = "https://cors-anywhere.herokuapp.com/";
+    const endPoint = "https://www.hikingproject.com/data/get-campgrounds?";
+    let url = `${cors}${endPoint}latitude=${latitude}&longitude=${longitude}&maxDistance=50&key=${API_KEY}&maxResults=2&sort=distance`;
+
+    request.onload = function() {
+      if (this.status === 200) {
+        resolve(request.response);
+      }
+      else {
+        reject(Error(request.statusText));
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
+  });
+}
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { findTrails } from './apis/trails';
 import { findWeather } from './apis/weather';
-//import { findCampgrounds } from './apis/camping';
+import { findCampgrounds } from './apis/camping';
 // import { findDistance } from './apis/distance';
 
 let origins;
@@ -16,6 +16,10 @@ function getLocation(location) {
 $(document).ready(function() {
   $("#searchNearby").click(function() {
     $("#hikesNearby").slideDown();
+    $("#intro").hide();
+  });
+  $("#searchCampgrounds").click(function() {
+    $("#campgroundsNearby").slideDown();
     $("#intro").hide();
   });
   $("#searchDestination").click(function() {
@@ -51,7 +55,7 @@ $(document).ready(function() {
                 let temperature = weatherBody.daily.data[0].temperatureHigh;
                 temperature = parseInt(temperature);
 
-                $("#display-results").append(`<img src="${trails[i].imgSmallMed}"><br><br>${trails[i].name}<br><br>${trails[i].location}<br>${trails[i].length} mile hike<br>${weatherSummary}<br>${temperature} degree high<br>Difficulty: ${trails[i].difficulty}<br><br><a href="https://www.google.com/maps/dir/${origins}/${trailCoordinates}">Get Directions</a><p></p><br><hr>`);
+                $("#display-results").append(`<img src="${trails[i].imgSmallMed}"><br><br>${trails[i].name}<br><br>${trails[i].location}<br>${trails[i].length} mile hike<br>${weatherSummary}<br>${temperature} degree high<br>Difficulty: ${trails[i].difficulty}<br>Rating: ${trails[i].stars}<br><a href="https://www.google.com/maps/dir/${origins}/${trailCoordinates}">Get Directions</a><p></p><br><hr>`);
               },
               function(error) {
                 $("#display-results").append(`I am the error message: ${error.message}`);
@@ -64,13 +68,50 @@ $(document).ready(function() {
         $("#display-div").show();
       });
   });
-  $("#goHome").click(function() {
-    $("#resultsDiv").hide();
-    $("#intro").slideDown();
-  });
 
-  $("#goHome2").click(function() {
-    $("#searchByPlace").hide();
-    $("#intro").slideDown();
+  $("#find-campgrounds").click(function () {
+    $("#resultsDiv2").slideDown("ease");
+    $("#campgroundsNearby").hide();
+    let campDistance = $("input#campground-distance").val();
+    $("input#campground-distance").val("");
+    // let hikingDate = $('input#dateInput').val();
+    // $("input#dateInput").val("");
+
+    findCampgrounds()
+      .then((response) => {
+
+        $("#display-results2").empty();
+        const body = JSON.parse(response);
+        const campgrounds = body.campgrounds;
+        console.log(campgrounds);
+        for (let i in campgrounds){
+          if (campgrounds[i].length < maxCampDistance){
+
+            let campgroundCoordinates = `${campgrounds[i].latitude},${campgrounds[i].longitude}`;
+
+            // findWeather(campgroundCoordinates, hikingDate)
+            //   .then((response) => {
+            //
+            //     let weatherBody = JSON.parse(response);
+            //     let weatherSummary = weatherBody.daily.data[0].summary;
+            //     let temperature = weatherBody.daily.data[0].temperatureHigh;
+            //     temperature = parseInt(temperature);
+
+                $("#display-results2").append(`${campgrounds[i].name}<br>Bookable Campground: ${campgrounds[i].isBookable}<br>Campsites: ${campgrounds[i].numCampsites}<br>${campgrounds[i].location}<br>${campgrounds[i].latitude},${campgrounds[i].longitude}<br><a href="https://www.google.com/maps/dir/${origins}/${campgroundCoordinates}">Get Directions</a><p></p><br><hr>`);
+              },
+              function(error) {
+                $("#display-results2").append(`I am the error message: ${error.message}`);
+              });
+          }
+        
+      },
+      function(error) {
+        $("#display-results2").empty().append(`<h5>There was an error processing your request: ${error.message}</h5>`);
+        $("#display-div2").show();
+      });
+
+  });
+  $("#homeButton").click(function() {
+    window.location.reload()
   });
 });
