2ec49eb3ab7ec6c717d2645ca08f39aa30bee542
Student <example@example.com>
2019-12-04 16:53:39 -0800

    Add progress

--- src/apis/weather.js
+++ src/apis/weather.js
@@ -1,13 +1,12 @@
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 
-export function findWeather(coordinates) {
+export function findWeather(coordinates, inputDate) {
   return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
 
-
     let unixTime = "1575468000";
-    let date = "2019-12-04";
+    let date = inputDate;
 
     const cors = "https://cors-anywhere.herokuapp.com/";
     let url = `${cors}https://api.darksky.net/forecast/${WEATHER_API_KEY}/${coordinates},${unixTime}?date=${date}`;
--- src/main.js
+++ src/main.js
@@ -9,48 +9,44 @@ import { findWeather } from './apis/weather';
 
 
 $(document).ready(function() {
-
   $("#searchNearby").click(function() {
     $("#hikesNearby").slideDown();
     $("#intro").hide();
   });
-
   $("#searchDestination").click(function() {
     $("#searchByPlace").slideDown();
     $("#intro").hide();
   });
-
   $("#find-trails").click(function () {
     $("#resultsDiv").slideDown("ease");
     $("#hikesNearby").hide();
     let maxHikeDistance = $("input#trail-distance").val();
     $("input#trail-distance").val("");
-
+    let hikingDate = $('input#dateInput').val();
+    $("input#dateInput").val("");
     findTrails()
       .then((response) => {
         $("#display-results").empty();
-
         const body = JSON.parse(response);
         const trails = body.trails;
         for (let i in trails){
           if (trails[i].length < maxHikeDistance){
-
             let trailCoordinates = `${trails[i].latitude},${trails[i].longitude}`;
-
             /* Dark Sky Weather API. Trail coordinates are used as arguments to find weather for that area. */
-
-            findWeather(trailCoordinates)
+            findWeather(trailCoordinates, hikingDate)
               .then((response) => {
-
                 let weatherBody = JSON.parse(response);
                 let weatherSummary = weatherBody.daily.data[0].summary;
                 let temperature = weatherBody.daily.data[0].temperatureHigh;
-                $("#display-results").append(`${trails[i].name}<br>${trails[i].location}<br>${trails[i].length} mile hike<br>${trails[i].latitude}, ${trails[i].longitude}<br>${weatherSummary}<br>${temperature} degree high<p></p><hr>`);
-              },
-
-              function(error) {
-                console.error(`I am the error message: ${error.message}`);
-              });
+                findDistance(currentLocation)
+                  .then((response) => {
+                    let locationBody = JSON.parse(response);
+                    let currentLocation = getLocation();
+                      $("#display-results").append(`${trails[i].name}<br>${trails[i].location}<br>${trails[i].length} mile hike<br>${trails[i].latitude}, ${trails[i].longitude}<br>${weatherSummary}<br>${temperature} degree high<a href="https://www.google.com/maps/dir/${currentLocation}/${trailCoordinates}">Get Directions</a><p></p><hr>`);
+              }
+              // function(error) {
+              //   console.error(`I am the error message: ${error.message}`);
+              // });
           }
         }
       },
@@ -58,18 +54,19 @@ $(document).ready(function() {
         $("#display-results").empty().append(`<h5>There was an error processing your request: ${error.message}</h5>`);
         $("#display-div").show();
       });
+    });
   });
   $("#goHome").click(function() {
     $("#resultsDiv").hide();
     $("#intro").slideDown();
-    // $("#display-results").html('');
   });
-
+  
   $("#goHome2").click(function() {
     $("#searchByPlace").hide();
     $("#intro").slideDown();
   });
 });
+
 
 
 
