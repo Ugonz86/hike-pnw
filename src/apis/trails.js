33b8d6e72f3e868d199d17c5d7c59d458a1e69e7
Ugonz86 <ugonzalez86@gmail.com>
2019-12-09 10:41:54 -0800

    Add new version of the project to new repo.

--- src/apis/trails.js
+++ src/apis/trails.js
@@ -5,7 +5,7 @@ export function findTrails(){
     let request = new XMLHttpRequest();
 
     const endPoint = "https://www.hikingproject.com/data/get-trails?";
-    let url = `${endPoint}lat=47.6062&lon=-122.3321&maxDistance=10&key=${API_KEY}&maxResults=3`;
+    let url = `${endPoint}lat=47.6062&lon=-122.3321&maxDistance=10&key=${API_KEY}&maxResults=10`;
 
     request.onload = function() {
       if (this.status === 200) {
